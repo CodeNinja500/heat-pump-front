f03f7d87ff3d26cf025346ca63256e6abf04112c
Maciej Paluch <maciekpal@gmail.com>
2025-01-13 20:25:58 +0100

    Angular material installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, RouterOutlet, CommonModule, HttpClientModule],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
